c6556e551b423b14897c8a52e2ba5abddfef5917
Sean Nelson <smnelsonn@gmail.com>
2019-02-25 17:40:44 -0800

    setup and early CSS

---
+++ test/tests.js
@@ -0,0 +1,21 @@
+const test = QUnit.test;
+
+const images = {
+    title: 'UniWhal', 
+    url: 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'
+};
+
+test('create html template for list items', function(assert){
+    //arrange
+    const expected = `
+    <li>
+        <p>UniWhal</p>
+        <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg">
+    </li>
+`
+
+    //act
+    const result = createHtmlTemplate(images);
+    //assert
+    assert.equal(result, expected);
+});
