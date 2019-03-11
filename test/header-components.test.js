32bfc5ff2e28f8dafa072d201ee4db1798797664
Sean Nelson <smnelsonn@gmail.com>
2019-03-11 11:54:47 -0700

    profile test, static, green

--- test/header-components.test.js
+++ test/header-components.test.js
@@ -13,17 +13,52 @@ function makeHeader(){
     template.innerHTML = html;
     return template.content;
 }
+
 test('display static section of header', assert => {
     //arrange
     const expected = `
+    <div>
+    <img src="./assets/readingRainbow.jpg" alt="website logo">
+    <h1>Book List</h1>
+    </div>
+    `;
+    
+    //act
+    const result = makeHeader();
+    
+    //assert
+    assert.htmlEqual(result, expected);
+});
+
+function makeProfile(user) {
+    const html = `
         <div>
-        <img src="./assets/readingRainbow.jpg" alt="website logo">
-        <h1>Book List</h1>
+            <span>Phoebe</span>
+            <img src="./phoebe">
+        </div>
+        `;
+
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
+}
+
+test('append user\'s data into header', assert => {
+    //arrange
+    const user = {
+        displayName: 'Phoebe',
+        photoURL: './phoebe'
+    };
+
+    const expected = `
+        <div>
+            <span>Phoebe</span>
+            <img src="./phoebe">
         </div>
     `;
 
     //act
-    const result = makeHeader();
+    const result = makeProfile(user);
 
     //assert
     assert.htmlEqual(result, expected);
