1d5b716d2f4b4ef880c3c8822f0edadef665a24c
Sean Nelson <smnelsonn@gmail.com>
2019-03-11 16:23:47 -0700

    readToQuery test passed

--- test/hash-query.test.js
+++ test/hash-query.test.js
@@ -4,11 +4,21 @@ QUnit.module('hash query interaction');
 const currentQuery = '';
 const searchTerm = 'rick';
 
-function writeToQuery(searchTerm){
+function writeToQuery(searchTerm) {
     const searchParams = new URLSearchParams(currentQuery);
     searchParams.set('name', searchTerm);
     searchParams.set('page', 1);
     return searchParams.toString();
+}
+// window.location.hash.slice(1) -> 'name=rick&page=1'
+// const currentQuery = window.location.hash.slice(1);
+function readFromQuery(hash) {
+    const searchParams = new URLSearchParams(hash);
+    const queryOptions = {
+        name: searchParams.get('name'),
+        page: Number(searchParams.get('page'))
+    };
+    return queryOptions;
 }
 
 test('write search term to query', assert => {
@@ -18,3 +28,13 @@ test('write search term to query', assert => {
 
     assert.equal(result, expected);
 });
+
+test('read search from query', assert => {
+    const hash = 'name=rick&page=1';
+    const expected = {
+        name: 'rick',
+        page: 1
+    };
+    const result = readFromQuery(hash);
+    assert.deepEqual(result, expected);
+})
